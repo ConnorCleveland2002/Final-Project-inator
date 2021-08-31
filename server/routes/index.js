fbc9c180266ef1f53e5568844b20c91efb75878d
ConnorCleveland2002 <connorcleveland2002@gmail.com>
2021-08-31 19:05:11 -0400

    Patchy functionality2

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,5 +7,8 @@ router.use("/api", apiRoutes);
 // router.use((req, res) => {
 //   res.sendFile(path.join(__dirname, "../../client/public/index.html"));
 // });
+router.get("/", async (req, res) => {
+  res.render("../../client/public/index.html")
+});
 
 module.exports = router;
