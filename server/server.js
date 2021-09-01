1b8e07298ba65e567a91f4daf8a4a16d149fcdce
ConnorCleveland2002 <connorcleveland2002@gmail.com>
2021-08-31 21:33:32 -0400

    Herokupush 3

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use((req, res, next) => {
+app.use("*", (req, res, next) => {
   if (req.originalUrl === "/graphql") return next();
     res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
